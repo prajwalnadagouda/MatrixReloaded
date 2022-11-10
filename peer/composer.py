f95c8f024b2d1d130c2ab82ef603d02b3eadf3b4
prajwalnadagouda <prajwalnadagouda@Prajwals-MacBook-Pro.local>
2022-11-09 17:55:04 -0800

    Fixed:Adding peer

--- peer/composer.py
+++ peer/composer.py
@@ -4,7 +4,7 @@ import sys
 
 p1=randint(10000, 65536)
 p2=randint(10000, 65536)
-p3=randint(10000, 65536)
+# p3=randint(10000, 65536)
 
 try:
     from configparser import ConfigParser
@@ -15,11 +15,11 @@ config = ConfigParser()
 config.add_section('ports')
 config.set('ports', '5000', str(p1))
 config.set('ports', '2006', str(p2))
-config.set('ports', '2007', str(p3))
+# config.set('ports', '2007', str(p3))
 config.add_section('host')
 config.set('host', 'id', str(sys.argv[1]))
 
-with open('ports.ini', 'w') as configfile:
+with open('info.ini', 'w') as configfile:
     config.write(configfile)
 
 names_yaml = """
@@ -31,12 +31,12 @@ services:
         #     - db
         ports:
             - "{}:5000"
-            - "{}:2006"
-            - "{}:2007"
+            - "{}:2008"
+            #- "{}:2007"
         networks:
             - peercmpe273
         volumes:
-            - ./ports.ini:/app/ports.ini
+            - ./info.ini:/app/info.ini
 
 networks:
     peercmpe273:
@@ -46,7 +46,7 @@ networks:
             config:
                 - subnet: 172.59.0.0/16
 """
-names_yaml=names_yaml.format(p1,p2,p3)
+names_yaml=names_yaml.format(p1,p2)#,p3)
 names = yaml.safe_load(names_yaml)
 with open('docker-compose.yml', 'w') as file:
     yaml.dump(names, file)
--- peer/peer.py
+++ peer/peer.py
@@ -1,16 +1,28 @@
 import socket
+from _thread import *
 from threading import Thread
 from time import sleep
-
+import configparser
 import socket
-print(([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]))
+import sys
+# print(([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]))
+
 
 
 class peer:
+    def server_communication(ClientMultiSocket):
+        while True:
+            Input = input('Hey there: ')
+            ClientMultiSocket.send(str.encode(Input))
+            res = ClientMultiSocket.recv(1024)
+            print(res.decode('utf-8'))
+        ClientMultiSocket.close()
+
     def connect_server():
+        config = configparser.ConfigParser()
+        config.read('info.ini')
         ClientMultiSocket = socket.socket()
-        # host = '127.0.0.1'
-        host = '10.10.10.10'
+        host = str(config['host']['id'])
         port = 2006
         print('Waiting for connection response')
         try:
@@ -23,14 +35,54 @@ class peer:
             print("Peer added to the list")
         else:
             print("Couldn't connect. Please retry later")
-        while True:
-            Input = input('Hey there: ')
-            ClientMultiSocket.send(str.encode(Input))
-            res = ClientMultiSocket.recv(1024)
-            print(res.decode('utf-8'))
-        # ClientMultiSocket.close()
+            return
+        Input=config['ports']['5000']
+        ClientMultiSocket.send(bytes(Input, 'utf-8'))
+        res = ClientMultiSocket.recv(1024)
+        peer.server_communication(ClientMultiSocket)
+        
 
     def peer_to_peer():
+        return
+        peer_list=[]
+        ServerSideSocket = socket.socket()
+        host = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
+        print(host)
+        port = 2008
+        thread_count = 0
+        try:
+            ServerSideSocket.bind((host, port))
+        except socket.error as e:
+            print(str(e))
+        print('Peer Socket is listening..')
+        ServerSideSocket.listen(5)
+        def multi_threaded_client(connection,address):
+            connection.send(str.encode('Server is working:'))
+            while True:
+                try:
+                    data = connection.recv(2048)
+                    response = 'Peer Server message: ' + data.decode('utf-8')
+                    if not data:
+                        break
+                    connection.sendall(str.encode(response))
+                except:
+                    print("connection closed")
+            connection.close()
+            peer_list.remove(address)
+            print(peer_list)
+        while True:
+            Client, address = ServerSideSocket.accept()
+            peer_list.append(address)
+            Client.sendall(b"Approved")
+            print('Connected to: ' + address[0] + ':' + str(address[1]))
+            start_new_thread(multi_threaded_client, (Client, address))
+            thread_count += 1
+            print('Thread Number: ' + str(thread_count))
+            print(peer_list)
+            # break
+        ServerSideSocket.close()
+
+    def matrix_multiplier():
         pass
 
 def wassup():
@@ -40,7 +92,7 @@ def wassup():
 
 peer_tracker = Thread(target=peer.connect_server, daemon=True, name='peer tracker')
 peer_tracker.start()
-wasss = Thread(target=wassup, daemon=True, name='hello boy')
+wasss = Thread(target=peer.peer_to_peer, daemon=True, name='hello boy')
 wasss.start()
 
 peer_tracker.join()
